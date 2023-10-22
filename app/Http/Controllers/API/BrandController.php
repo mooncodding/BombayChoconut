3ba80b6abe39220b76344e04ff266f0ebb272b38
Asad Amjad <asadamjad922@gmail.com>
2023-10-23 00:52:30 +0500

    changes

--- app/Http/Controllers/API/BrandController.php
+++ app/Http/Controllers/API/BrandController.php
@@ -13,6 +13,7 @@ class BrandController extends Controller
     public function __construct(){
         $this->middleware(['auth', 'permission:brands']);
     }
+    
     /**
      * Display a listing of the resource.
      *

bc72accb71c252a42f5f967d06bf7037dd529711
Asad Amjad <asadamjad922@gmail.com>
2024-05-13 20:50:41 +0500

    asad commit

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $table = 'tags';
+
+    protected $fillable = [
+        'name', 
+        'created_by', 
+        'updated_by',
+    ];
+
+    public function createdBy(){
+        return $this->belongsTo('App\Models\User', 'created_by');
+    }
+
+    public function updatedBy(){
+        return $this->belongsTo('App\Models\User', 'updated_by');
+    }
 }
